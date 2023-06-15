6788bcebc2c0fa8eb7ad65db572e6eb572105d8e
BhuviGovind <bhuvanaammu08@gmail.com>
2023-06-15 14:43:22 +0530

    Added TC003_CreateLead.java

---
+++ src/main/java/com/leaftaps/testcases/TC003_CreateLead.java
@@ -0,0 +1,29 @@
+package com.leaftaps.testcases;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.leaftaps.pages.LoginPage;
+import com.leaftaps.testngbase.TestNgBase;
+
+public class TC003_CreateLead extends TestNgBase {
+	
+	@BeforeTest
+	public void setValues() {
+		testCaseName = "TC003_CreateLead";
+		testDescription = "Creating a Lead";
+		nodes = "Login Module";
+		authors = "Aravindraj";
+		category = "Smoke";
+		excelDataFileName = "TC002_Login";
+	}
+	
+	@Test(dataProvider="fetchData")
+	public void loginToLeafTaps(String userName, String password) {
+		new LoginPage(driver, node, test)
+		.enterUserName(userName)
+		.enterPassword(password)
+		.clickLoginButton();
+	}
+
+}
